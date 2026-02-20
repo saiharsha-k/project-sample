62f773bb7ce39f81af8a40ab3c3bef3b1b277e4c
saiharsha-k <harsha@datavalley.ai>
2026-02-20 11:02:02 +0000

    Created repo, README and prime check program file

---
+++ README.md
@@ -0,0 +1,3 @@
+### This is a repository for the code of the demo project. 
+
+The project is a simple web application that allows users to create and manage their to-do lists. The application is built using React for the frontend and Node.js for the backend.
---
+++ prime_check.py
@@ -0,0 +1,20 @@
+print("Hello, world!")
+
+print("This is a sample Python script.")
+
+def check_prime(name):
+    if name < 2:
+        return False
+    for i in range(2, int(name**0.5) + 1):
+        if name % i == 0:
+            return False    
+    return True
+
+number = 29
+if check_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
+
+if __name__ == "__main__":
+    print("This code is running as the main program.")
